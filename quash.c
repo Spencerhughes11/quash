fc94580f494681c2c5683383f2db27982d7312c9
Spencer Hughes <spencerhughes16@gmail.com>
2023-10-18 23:19:17 -0500

    Implement exit/quit and pwd commands

--- quash.c
+++ quash.c
@@ -2,11 +2,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 static char char_in;
+char input[1024];
+
+char *argv[];
+int argc;
+
 // prints shell line
 void print_quash(){
-    printf("[QUASH]$ ");
+    printf("\n[QUASH]$ ");
+}
+
+// prints pwd
+void run_pwd() {
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    printf("%s\n", cwd);
+}
+
+void run_cd() {
+    if (argv[1] == NULL || argv[1] == '~'){
+        chdir(getenv("HOME"));
+    }
+}
+
+int run_commands(){
+    fgets(input, sizeof(input), stdin);
+    input[strlen(input) - 1] = '\0';
+
+    // exit or quit prompt
+    
+    if (strcmp("exit", input) == 0 || strcmp(input, "quit") == 0 ){
+        printf("Exiting quash...\n\n");
+        exit(0);
+    }
+
+    // if (strcmp("#", input) == 0){
+    //     return 0;
+    // }
+
+    // prints cd
+    if (strcmp("cd", input) == 0) {
+        run_cd();
+        run_pwd();
+        return 0;
+    }
+    // prints pwd
+    if (strcmp("pwd", input) == 0) {
+        run_pwd();
+        return 0;
+    }
 }
 
 int main (int argc, char *argv[]) 
@@ -30,16 +77,19 @@ int main (int argc, char *argv[])
 //      "QQQQQQQQ::::QQ      UUUUUUUUU  AAAAAAA                   AAAAAAASSSSSSSSSSSSSSS   HHHHHHHHH     HHHHHHHHH"
 //              "Q:::::Q"                                                                                         
 //               "QQQQQQ\n\n\r");
-    printf("\n\nWelcome to Quash!\n\nPress [Enter] to begin...\n\n");
+    printf("\n\nWelcome to Quash!\n\n");
     while(1){
-        char_in = getchar();
+        print_quash();
         
-        if (char_in == '\n'){
+        // char_in = getchar();
+        fgets(input, sizeof(input), stdin);
+        input[strlen(input) - 1] = '\0';
+        
+        if (input == ' '){
             print_quash();
-            fflush(stdout);
         } else {
-            // print_quash();
-            printf("\n");
+            print_quash();
+            run_commands();
         }
     }
     return 0;

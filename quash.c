071c1a9c6eec83d9ec9bc744683f973c0e2b9552
Spencer Hughes <spencerhughes16@gmail.com>
2023-10-25 00:26:33 -0500

    Implement echo function with basic strings and double quotes

--- quash.c
+++ quash.c
@@ -53,13 +53,29 @@ void run_ls() {
     closedir(dir);      // closes directory
 }
 
-void run_echo(){
-    
+// * FIXME: implement path echoes
+// * doesn't remove single quotes 
+
+void run_echo(int argc){
+    for (int i = 1; i < argc; i++){
+        if (strcmp("#", argv[i]) == 0){
+            break;      // ignores comments
+        }
+
+        if ((argv[i][0] == "'") || (argv[i][0] == '"') ){       // handles if string begins with single or double quote
+            // argv[i] = argv[i+1];
+            argv[i] = argv[i] + 1;      // skips
+        }
+        if ((argv[i][strlen(argv[i]) - 1] == "'") || (argv[i][strlen(argv[i]) - 1] == '"') ){       // handles if string ends with single or double quote
+            argv[i][strlen(argv[i]) - 1] ='\0';         // sets to null
+        }
+        printf("%s ", argv[i]);
+    }
+    printf("\n");
 }
+
 void run_execution() {
     pid_t pid = fork();
-    printf(argv[0]);
-
 
     if (pid == -1) {
         // Fork failed
@@ -81,22 +97,25 @@ void run_execution() {
 }
 
 /* COMMAND LINE PARSER 
- *    stores argc and argv values to access
+ *   stores argc and argv values to access
  *   command line input outside of main
 */
 void parse_command_line() {
-    int argc = 0;
+    argc = 0;
     char *token = strtok(input, " ");
     while (token != NULL) {
         argv[argc++] = token;
         token = strtok(NULL, " ");
     }
     argv[argc] = NULL;
+    
 }
 
 int run_commands(){
     fgets(input, sizeof(input), stdin);
     input[strlen(input) - 1] = '\0';
+    
+    // parse_command_line();
     int argc = 0;
     char *token = strtok(input, " ");
     while (token != NULL) {
@@ -104,19 +123,16 @@ int run_commands(){
         token = strtok(NULL, " ");
     }
     argv[argc] = NULL;
-
-    // parse_command_line();
-    // exit or quit prompt
-    if (strcmp("&",argv[argc - 1]) == 0){
+    
+    if (strcmp("&", argv[argc - 1]) == 0){
         // dir = opendir(".");         // opens current directory
-        argv[argc - 1] == NULL;
+        argv[argc - 1] = NULL;
         run_execution();
         return 0;
-        
-
-        
+     
     }
     
+    // exit or quit prompt
     if (strcmp("exit", argv[0]) == 0 || strcmp(argv[0], "quit") == 0 ){
         printf("Exiting quash...\n\n");
         exit(0);
@@ -143,7 +159,7 @@ int run_commands(){
         return 0;
     }
     if (strcmp("echo", argv[0]) == 0) {
-        run_echo();
+        run_echo(argc);
         return 0;
     }
     // clear input arr for each new iteration
@@ -171,9 +187,9 @@ int main (int argc, char *argv[])
             
 
         }
-        for (int i = 0; i < argc; i++) {
-                argv[i] = NULL;
-        }
+        // for (int i = 0; i < argc; i++) {
+        //         argv[i] = NULL;
+        // }
     }
     return 0;
 }

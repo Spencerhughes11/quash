2e024892f6798818284f0b46c5cad329f9f5c566
pete Junge <petejunge7@gmail.com>
2023-10-31 20:38:18 -0500

    needed run_pipe

---
+++ program1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+    
+    // while(1){
+    //     int hi;
+    // }
+    
+    sleep(2);
+    STDOUT_FILENO = "Hello world";
+    // fflush(stdout);
+    return STDOUT_FILENO;
+}
+
+   
---
+++ program3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    
+    // while(1){
+    //     int hi;
+    // }
+    
+    sleep(100);
+
+    return 0;
+}
